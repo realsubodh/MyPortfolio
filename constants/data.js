b775dcc1cd917cbb47f393ce5beee375d644493f
realsubodh <subodhsingh3477@gmail.com>
2024-11-02 03:30:54 +0530

    adding up the files

---
+++ constants/data.js
@@ -0,0 +1,80 @@
+const userData = {
+  githubUsername: "realsubodh",
+  name: "Subodh Singh",
+  designation: "Software Developer",
+  avatarUrl: "/avatar.png",
+  email: "subodhsingh0021@gmail.com",
+  phone: "+91 9793767755",
+  address: "Varanasi, Uttar Pradesh, India.",
+  projects: [
+    {
+      title: "PostStream",
+      link: "https://github.com/realsubodh/PostStream",
+      imgUrl: "/PostStream.jpg",
+    },
+    {
+      title: "QuickSnips",
+      link: "https://github.com/realsubodh/QuickSnips",
+      imgUrl: "/QuickSnip.png",
+    },
+    {
+      title: "LeetTrackr",
+      link: "https://leet-trackr.vercel.app/",
+      imgUrl: "/LeetTrackr.png",
+    },
+    {
+      title: "RateSync",
+      link: "https://ratesync.netlify.app/",
+      imgUrl: "/RateSync.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Software Developer with a mission to create digital experiences and products that make a difference",
+    description: [
+      `I embarked on my coding journey four years ago during the lockdown, diving first into C programming and quickly progressing to Python and C++. My passion for Data Structures and Algorithms has been pivotal in building a solid technical foundation, which I continue to develop alongside my CSE studies at LPU.`,
+      `From the beginning, web development captured my interest. I’ve honed my skills in HTML, CSS, JavaScript, and, more recently, React.js and Tailwind CSS—crafting seamless, responsive interfaces has become second nature to me. I've worked on a range of projects for myself, classmates, and clients, each reinforcing my expertise in frontend technologies.`,
+       `As I push forward, I’m excited to expand into backend development to complete the stack and fully realize my goal of becoming a full-stack developer with a strong command of both frontend and algorithmic problem-solving.`,
+    ],
+  },
+  experience: [
+    
+    {
+      title: "Frontend Developer Intern",
+      company: "Sanyam Trust",
+      year: "2023",
+      desc: "Started my journey as a Intern. Learned the ropes of real-world coding and brought designs to life one line at a time.",
+    },
+    {
+      title: "Graduation",
+      company: "Lovely Professional University",
+      year: "2021",
+      companyLink: "https://www.lpu.in/",
+      desc: "Majoring in CSE at LPU has been a defining journey. It’s where I upgraded from curious coder to someone who actually knows what they’re doing (mostly).",
+    },
+    {
+      title: "Class 12th",
+      company: "Rainbow Modern School, Varanasi.",
+      year: "2021",
+      desc: "Scored 82% in Class 12th, courtesy of a lockdown grading system and a commerce teacher who didn’t exactly see eye-to-eye with my brilliance.",
+    },
+    {
+      title: "Class 10th",
+      company: "Rainbow Modern School, Varanasi.",
+      year: "2019",
+      desc: "Somehow I managed to stumble my way to 92% in Class 10th. The struggle was real, folks.",
+    },
+  ],
+  resumeUrl:
+    "https://magenta-janey-30.tiiny.site",
+  socialLinks: {
+    instagram: "https://instagram.com/rea_lsubodh",
+    twitter: "https://twitter.com/real_subodh",
+    linkedin: "https://linkedin.com/in/realsubodh",
+    github: "https://github.com/realsubodh",
+    peerlist: "https://peerlist.io/realsubodh",
+    leetcode:"https://leetcode.com/u/realsubodhsingh/",
+  },
+};
+
+export default userData;
